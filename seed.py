92743f8cc08f8d5e5a70cd69928ec5bdbceec83c
Romain Komorn <romain.komorn@hackbrightacademy.com>
2018-07-16 15:48:30 -0700

    for fun

--- seed.py
+++ seed.py
@@ -6,10 +6,10 @@ from model import connect_to_db, db, Cat
 def seed_cats():
     """Seed cats."""
 
-    auden = Cat(name="Auden")
-    ezra = Cat(name="Ezra")
-    db.session.add(auden)
-    db.session.add(ezra)
+    azrael = Cat(name="Azrael")
+    tom = Cat(name="Tom")
+    db.session.add(azrael)
+    db.session.add(tom)
     db.session.commit()
 
 if __name__ == "__main__":
